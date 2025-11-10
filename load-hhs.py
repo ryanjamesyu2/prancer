d5f8caed27c5f85dce07ad0a12e983678d5b8717
Ryan Yu <rjy2@andrew.cmu.edu>
2025-11-10 15:25:14 -0500

    Added empty python scripts for loading data

---
+++ load-hhs.py
@@ -0,0 +1,10 @@
+# Python script to load the HHS data set
+
+# Define helper functions
+def preprocess(data):
+    pass
+
+# Driver code to load data
+# Use try-except, with rollback in except to make sure no data is inserted if there's an error
+
+# Driver code to update hospital table if necessary
---
+++ load-quality.py
@@ -0,0 +1,10 @@
+# Python script to load the hospital quality data set
+
+# Define helper functions
+def preprocess(data):
+    pass
+
+# Driver code to load data
+# Use try-except, with rollback in except to make sure no data isinserted if there's an error
+
+# Driver code to update hospital table if necessary
