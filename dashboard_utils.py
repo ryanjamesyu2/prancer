29488f01c761b7213f126718821b8c3ad6fcf82c
Qianruixi Wang <wqianruixi@gmail.com>
2025-11-29 23:34:42 -0500

    Utilities for dashboard

---
+++ dashboard_utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import psycopg
+import credentials
+
+
+def get_connection():
+    return psycopg.connect(
+        host="debprodserver.postgres.database.azure.com",
+        dbname=credentials.DB_USER,
+        user=credentials.DB_USER,
+        password=credentials.DB_PASSWORD)
+
+
+def run_query(sql, params):
+    conn = get_connection()
+    try:
+        df = pd.read_sql(sql, conn, params=params)
+    finally:
+        conn.close()
+    return df
